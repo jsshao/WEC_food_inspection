5f87de8bf1bc9c573633c20e71623a63f8044b31
mhlu <mhlu@uwaterloo.ca>
2015-11-20 21:24:31 -0500

    changed restaurant violation to a template

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, make_response, request, send_from_directory
+from flask import render_template
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='/static')
@@ -8,6 +9,10 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
+
+@app.route('/restaurant/<rid>')
+def stores(rid):
+    return render_template('restaurant.html', name='ABCD')
 
 @app.route('/violation.html')
 def send_violations():
--- static/js/dataTable.js
+++ static/js/dataTable.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
 });
 
 function initialize() {
-var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-      center: new google.maps.LatLng(44.5403, -78.5463),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var pos = {lat: 43.4689, lng: -80.5400};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: pos
+    });
+
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: 'Address:'
+    });
 }
 window.addEventListener('load', initialize);
